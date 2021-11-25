bf141dfb6e3dd8e90c9d7720230e7f0123cf4821
Emmanuel Onyeyaforo <rahzy24@gmail.com>
2021-11-25 11:24:25 +0100

    learning node

---
+++ people.js
@@ -0,0 +1,7 @@
+const people = ['a', 'b', 'c', 'd', 'e'];
+const age = [1,2,3,4,5,6]
+
+
+module.exports = {
+    people, age
+}
---
+++ test.js
@@ -0,0 +1,4 @@
+const { age, people } = require('./people')
+
+console.log(age)
+console.log(people)
